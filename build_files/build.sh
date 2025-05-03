c56d6c8d19b7615044dbeb5618625bfc824b25e9
Adam Israel <adam@adamisrael.com>
2025-05-03 13:54:36 -0400

    Install additional packages

--- build_files/build.sh
+++ build_files/build.sh
@@ -12,8 +12,9 @@ set -ouex pipefail
 # this installs a package from fedora repos
 dnf5 install -y tmux
 
-# hyprlands
+# hyprland
 dnf5 install -y hyprland
+dnf5 install -y waybar hyprpaper dolphin
 
 # Use a COPR Example:
 #
