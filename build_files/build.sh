286d40c3be0ef50bf2fe6cf894ec6efa0624e389
Adam Israel <adam@adamisrael.com>
2025-05-20 12:28:57 -0400

    Add --skip-unavailable

--- build_files/build.sh
+++ build_files/build.sh
@@ -14,7 +14,7 @@ dnf5 install -y tmux
 
 # hyprland
 dnf5 install -y hyprland
-dnf5 install -y waybar hyprpaper dolphin dunst pavucontrol hypridle hyprlock
+dnf5 install --skip-unavailable -y waybar hyprpaper dolphin dunst pavucontrol hypridle hyprlock
 
 # Use a COPR Example:
 #
